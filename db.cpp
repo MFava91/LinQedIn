7fa07b784572a15d42bcc67234f5188e7befd758
shake91 <mattiafa91@gmail.com>
2015-01-13 22:38:59 +0100

    Aggiunta la possibilità di salvare una lista di utenti con un numero di utenti >1

--- db.cpp
+++ db.cpp
@@ -27,49 +27,51 @@ void DB::save() {
         std::cout << "File opened OK" << std::endl;
     }
     QXmlStreamWriter xmlWriter(&file);
-    std::map<QString,Utente*>::iterator it=dbUtenti.begin();
     xmlWriter.setAutoFormatting(true);
     xmlWriter.writeStartDocument();
-    xmlWriter.writeStartElement("Utente");
-    xmlWriter.writeStartElement("Profilo");
-    xmlWriter.writeStartElement("DatiAnagrafici");
-    xmlWriter.writeTextElement("Nome",(*it).second->info.datiPersonali.getNome());
-    xmlWriter.writeTextElement("Cognome",(*it).second->info.datiPersonali.getCognome());
-    xmlWriter.writeTextElement("Email",(*it).second->info.datiPersonali.getEmail());
-    xmlWriter.writeTextElement("Data",(*it).second->info.datiPersonali.getDataNascita().toString());
-    xmlWriter.writeTextElement("Luogo_Nascita",(*it).second->info.datiPersonali.getLuogoNascita());
-    xmlWriter.writeTextElement("Residenza",(*it).second->info.datiPersonali.getResidenza());
-    xmlWriter.writeEndElement();
-    xmlWriter.writeStartElement("Titoli_Studio");
-    xmlWriter.writeTextElement("Diploma",(*it).second->info.studi.getDiploma());
-
-    int sizeStudi=(*it).second->info.studi.getLaurea().size();
-    for (int i=0;i<sizeStudi;i++) {
-        xmlWriter.writeTextElement("Laurea",(*it).second->info.studi.getLaurea()[i]);
-    }
-
-    /*OLD
-    for (vector<QString>::iterator itv = (*it).second->info.studi.getLaurea().begin(); itv != (*it).second->info.studi.getLaurea().end(); ++itv) {
-        xmlWriter.writeTextElement("Laurea",*itv);
-    }
-    */
-    xmlWriter.writeEndElement();
-
-    xmlWriter.writeStartElement("Esperienze_Lavorative");
-    int sizeLavori=(*it).second->info.curriculum.getEsperienze().size();
-
-    //OLD  for (vector<Lavoro>::iterator itl = (*it).second->info.curriculum.getEsperienze().begin(); itl != (*it).second->info.curriculum.getEsperienze().end(); itl++)
-    for(int i=0;i<sizeLavori;i++)
+    for(std::map<QString,Utente*>::iterator it=dbUtenti.begin();it!=dbUtenti.end();++it)
     {
-
-        xmlWriter.writeStartElement("Lavoro");
-        xmlWriter.writeTextElement("Azienda",(*it).second->info.curriculum.getEsperienze()[i].getAzienda());
-        xmlWriter.writeTextElement("Titolo",(*it).second->info.curriculum.getEsperienze()[i].getTitolo());
-        xmlWriter.writeTextElement("Citta",(*it).second->info.curriculum.getEsperienze()[i].getCitta());
-        xmlWriter.writeTextElement("Inizio",(*it).second->info.curriculum.getEsperienze()[i].getInizio().toString());
-        xmlWriter.writeTextElement("Fine",(*it).second->info.curriculum.getEsperienze()[i].getFine().toString());
+        xmlWriter.writeStartElement("Utente");
+        xmlWriter.writeStartElement("Profilo");
+        xmlWriter.writeStartElement("DatiAnagrafici");
+        xmlWriter.writeTextElement("Nome",(*it).second->info.datiPersonali.getNome());
+        xmlWriter.writeTextElement("Cognome",(*it).second->info.datiPersonali.getCognome());
+        xmlWriter.writeTextElement("Email",(*it).second->info.datiPersonali.getEmail());
+        xmlWriter.writeTextElement("Data",(*it).second->info.datiPersonali.getDataNascita().toString());
+        xmlWriter.writeTextElement("Luogo_Nascita",(*it).second->info.datiPersonali.getLuogoNascita());
+        xmlWriter.writeTextElement("Residenza",(*it).second->info.datiPersonali.getResidenza());
         xmlWriter.writeEndElement();
+        xmlWriter.writeStartElement("Titoli_Studio");
+        xmlWriter.writeTextElement("Diploma",(*it).second->info.studi.getDiploma());
+
+        int sizeStudi=(*it).second->info.studi.getLaurea().size();
+        for (int i=0;i<sizeStudi;i++) {
+            xmlWriter.writeTextElement("Laurea",(*it).second->info.studi.getLaurea()[i]);
+        }
+
+        /*OLD
+        for (vector<QString>::iterator itv = (*it).second->info.studi.getLaurea().begin(); itv != (*it).second->info.studi.getLaurea().end(); ++itv) {
+            xmlWriter.writeTextElement("Laurea",*itv);
+        }
+        */
+        xmlWriter.writeEndElement();
+
+        xmlWriter.writeStartElement("Esperienze_Lavorative");
+        int sizeLavori=(*it).second->info.curriculum.getEsperienze().size();
+
+        //OLD  for (vector<Lavoro>::iterator itl = (*it).second->info.curriculum.getEsperienze().begin(); itl != (*it).second->info.curriculum.getEsperienze().end(); itl++)
+        for(int i=0;i<sizeLavori;i++)
+        {
+
+            xmlWriter.writeStartElement("Lavoro");
+            xmlWriter.writeTextElement("Azienda",(*it).second->info.curriculum.getEsperienze()[i].getAzienda());
+            xmlWriter.writeTextElement("Titolo",(*it).second->info.curriculum.getEsperienze()[i].getTitolo());
+            xmlWriter.writeTextElement("Citta",(*it).second->info.curriculum.getEsperienze()[i].getCitta());
+            xmlWriter.writeTextElement("Inizio",(*it).second->info.curriculum.getEsperienze()[i].getInizio().toString());
+            xmlWriter.writeTextElement("Fine",(*it).second->info.curriculum.getEsperienze()[i].getFine().toString());
+            xmlWriter.writeEndElement();
+        }
+        xmlWriter.writeEndElement();
+        xmlWriter.writeEndDocument();
     }
-    xmlWriter.writeEndElement();
-    xmlWriter.writeEndDocument();
 }
